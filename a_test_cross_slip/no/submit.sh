646d7e6c3fbb49cec3e7cb149e6b9016665cfe33
qzy157 <2231498022@qq.com>
2026-06-10 19:17:51 +0800

    test

---
+++ a_test_cross_slip/no/submit.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --partition=comp
+#SBATCH --job-name=load
+#SBATCH --nodes=1           # Request 1 node
+#SBATCH --ntasks=1          # Total number of tasks
+#SBATCH --cpus-per-task=48  # Number of CPU cores per task
+#SBATCH --time=48:00:00
+
+module purge
+module load miniforge/25.3.1
+source activate opendis_cpu
+python no.py
